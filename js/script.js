bbd0e958fe74b7fdbf38eda6b333542acc7508ce
MattiaLongo03 <mattilongo2003@gmail.com>
2022-10-24 16:12:29 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const eleSquares = document.querySelector('.squares');
 const eleTitle = document.querySelector('h1');
-const square = document.querySelector('.sq');
 
 eleTitle.addEventListener('click', function () {
 	for (let i = 1; i <= 100; i++) {
